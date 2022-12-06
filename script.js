3562fa61086b85e5cb59d24c316a684b0ea64ed6
Pedro Goulart <pedromgoulart@hotmail.com>
2022-12-06 19:55:00 -0300

    Adicionado outros exercicios

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //FUNÇÕES BÁSICAS//
-
+/*
 let areaNome = document.getElementById('bloco')
 
 
@@ -23,12 +23,12 @@ function saiu(){
     areaNome.innerText = 'Saiu do bloco'
     areaNome.style.background = 'green'
 
-}
+}*/
 
 
 
 
-
+/*
 function clicarSoma(){
 
     let txt1 = document.getElementById('txt1')
@@ -39,4 +39,48 @@ function clicarSoma(){
     let resultado = document.getElementById('resultado')
 
     resultado.innerHTML = `A soma de ${numero1} mais ${numero2} é ${total}`
+}*/
+
+
+
+
+
+//// FUNÇÕES COM PARAMETRO E RETORNO//////
+
+//Verificando se o número é maior ou menor que 100//
+
+/*
+function verificarNumero(n){
+
+    if(n => 100){
+
+        return 'Maior que 100'
+
+    }else{
+
+        return 'Menor que 100'
+
+    }
+
 }
+
+let resultado = verificarNumero(174)
+console.log(resultado)*/
+
+
+function soma(n1, n2){
+
+    if(n1 + n2 >= 7){
+
+        return 'Aprovado'
+
+    }else{
+
+        return 'Reprovado'
+
+    }
+
+}
+
+let notaFinal = soma(4, 8)
+console.log(notaFinal)
